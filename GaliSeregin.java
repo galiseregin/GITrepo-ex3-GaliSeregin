4a6f9782df475b493e2225665dc37614a2cd9c57
galiseregin <glygly135@gmail.com>
2024-05-21 11:38:18 +0300

    new_Upload

--- GaliSeregin.java
+++ GaliSeregin.java
@@ -1,7 +1,7 @@
 public class GaliSeregin{
 	public static void main(String[] args) {
 		for (int i=1;i<=10; i++) {
-			System.out.println("GaliSeregin"+i);
+			System.out.println("Gali_Seregin (="+i);
 		}
 	}
 }
